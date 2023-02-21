c9dd1992a0bcaf9979b6055dff0a3df2011cd448
JieYangZeng <jy0987953699@gmail.com>
2023-02-21 19:33:16 +0800

    『Product function』 - add getProductList function(add sorting)

--- src/main/java/com/jiyzen/springbootmall/controller/ProductController.java
+++ src/main/java/com/jiyzen/springbootmall/controller/ProductController.java
@@ -21,11 +21,17 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProductList(
+            //Filtering
             @RequestParam(required = false) ProductCategory category,
-            @RequestParam(required = false) String search) {
+            @RequestParam(required = false) String search,
+            //Sorting
+            @RequestParam(defaultValue = "last_modified_date") String orderBy,
+            @RequestParam(defaultValue = "desc") String sort) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
 
         List<Product> productList = productService.getProductList(productQueryParams);
 
--- src/main/java/com/jiyzen/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/jiyzen/springbootmall/dao/impl/ProductDaoImpl.java
@@ -42,6 +42,8 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
+        sql += " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
         List<Product> productList = npjt.query(sql, map, new ProductRowMapper());
 
         return productList;
